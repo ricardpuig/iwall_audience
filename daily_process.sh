2392dcc5dfca5ed718fbc473f95d8cdedcbbc97f
Ricard Puig <ricardpuig@Ricards-MacBook-Pro.local>
2021-02-15 10:09:12 +0100

    sh

--- daily_process.sh
+++ daily_process.sh
@@ -2,7 +2,7 @@
 #Audience Launcher
 source audience/bin/activate
 
-python3 bs-campaigns-all.py all peru 
-python3 bs-campaigns-all.py all colombia
-python3 bs-campaigns-all.py all spain
- 
+python3 bs-campaigns-report.py all peru 
+python3 bs-campaigns-report.py all colombia
+python3 bs-campaigns-report.py all spain
+
