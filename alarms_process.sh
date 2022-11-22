5675bcfaf5a3d2322da1bbb4f0b90fdb9beb2aaa
ricardpuig <rpuig@iwallinshop.com>
2022-11-22 14:19:26 +0100

    bug

--- alarms_process.sh
+++ alarms_process.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #Audience Launcher
-source audience/bin/activate
 
-python3 bs-campaigns-report.py all PERU
-python3 bs-campaigns-report.py all COLOMBIA
-python3 bs-campaigns-report.py all SPAIN
+python3 /home/ubuntu/iwall_audience/V2/monitoring/player_monitoring.py SPAIN
 
